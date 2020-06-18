eacb743d232a3550f2414283d4e819fb32532de4
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-17 20:16:22 -0400

    npm i + add boilerplate to accountsRouter, export accountsRouter + import accountsRouter to server.js

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -4,3 +4,31 @@ const express = require('express');
 const knex =  require('../data/dbConfig.js');
 
 const router = express.Router();
+
+// CRUD endpoints
+
+// Create
+router.post('/', (req, res) => {
+
+})
+ 
+// Read
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+// Update
+router.put('/:id', (req, res) => {
+
+})
+
+// Delete
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router;
