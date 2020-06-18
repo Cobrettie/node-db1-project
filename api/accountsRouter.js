48c0d6bc468bc1edad8d6a42d8bc93bbc5119529
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-17 21:25:39 -0400

    configure endpoints to get all acounts + get account by id + create new account

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -8,21 +8,68 @@ const router = express.Router();
 // CRUD endpoints
 
 // Create
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const accountData = req.body;
 
+  try {
+    const numberOfAccounts = await knex('accounts').insert(accountData)
+    res.status(201).json(numberOfAccounts);
+  } catch (err) {
+    console.log("Error: ", err);
+    res.status(500).json({
+      errorMessage: "Failed to create new account"
+    })
+  }
 })
- 
+
 // Read
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
+//   knex('accounts')
+//     .then(accounts => {
+//       res.status(200).json(accounts)
+//     })
+//     .catch(err => {
+//       console.log("Error: ", err)
+//       res.status(500).json({
+//         errorMessage: "Problem with db"
+//       })
+//     })
+// })
 
+router.get('/', async (req, res) => {
+  try {
+    const accounts = await knex('accounts');
+    res.status(200).json(accounts)
+  } catch (err) {
+    console.log("Error: ", err)
+    res.status(500).json({
+      errorMessage: "Problem with db"
+    })
+  }
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const account = await knex('accounts').where({
+      id: id
+    })
+    res.status(200).json(account)
+  } catch (err) {
+    console.log("Error: ", err);
+    res.status(500).json({
+      errorMessage: "Could not retrieve account"
+    })
+  }
 })
 
 // Update
 router.put('/:id', (req, res) => {
+  // try{
+    
+  // } catch (err) {
+
+  // }
 
 })
 
